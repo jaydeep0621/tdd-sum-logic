2b5503c8dc98ac657f084a5403854b3877464c6f
jaydeep0621 <jaydeep0216@gmail.com>
2025-01-03 00:09:56 +0530

    single number test

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const add = require('./sum');
 test('empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
+
+test('single number should return itself', () => {
+  expect(add('1')).toBe(1);
+});
