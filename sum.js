e81fd6b299816c5701fc3681fc6b239b235eb4c7
jaydeep0621 <jaydeep0216@gmail.com>
2025-01-03 00:12:10 +0530

    return sum of multiple number

--- sum.js
+++ sum.js
@@ -2,7 +2,8 @@ function add(numbers) {
   if (numbers === '') {
     return 0;
   }
-  return parseInt(numbers, 10)
-}
+  const nums = numbers.split(',');
+  return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
+};
 
 module.exports = add;
--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,10 @@ test('empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
 
-test('single number should return itself', () => {
+test('return number itself when single number', () => {
   expect(add('1')).toBe(1);
+});
+
+test('return sum of multiple number', () => {
+  expect(add('1,2,3')).toBe(6);
 });
